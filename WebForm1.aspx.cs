a733587ad85df7ed074952c1c8f16aba43cbf397
Karimaelkadiri <78538709+Karimaelkadiri@users.noreply.github.com>
2021-03-03 18:04:41 +0100

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                  int i =Convert.ToInt32 (TextBox2.Text);
+
+                int L = Convert.ToInt32(TextBox3.Text);
+                   string nom = TextBox1.Text;
+                   string marceau = nom.Substring(i, L);
+
+                 Label2.Text = "Salam elikom la partie du nom est :" + marceau;
+            }catch(Exception ex)
+            {
+                Response.Write("erreur"+ex.Message);
+            }
+          
+
+        }
+    }
+}
