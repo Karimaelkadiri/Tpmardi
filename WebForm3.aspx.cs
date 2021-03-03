00cbabd92de7d1aa77e10dc373a4163b34395fd2
Karimaelkadiri <78538709+Karimaelkadiri@users.noreply.github.com>
2021-03-03 18:10:01 +0100

    Add files via upload

---
+++ WebForm3.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+namespace WebApplication5
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            testchangecouleur();
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            testchangecouleur();
+
+        }
+        public void testchangecouleur()
+        {
+            if (RadioButtonList1.SelectedIndex == 0)
+            {
+                Panel1.BackColor = Color.Red;
+            }
+            if (RadioButtonList1.SelectedIndex == 1)
+            {
+                Panel1.BackColor = Color.Blue;
+            }
+            if (RadioButtonList1.SelectedIndex == 2)
+            {
+                Panel1.BackColor = Color.Green;
+            }
+
+        }
+    }
+}
